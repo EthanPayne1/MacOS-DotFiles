9a4e0a28bdb848888c8d85f7814280d575cbceaa
EthanPayne1 <payne.ethan6@yahoo.com>
2024-10-09 20:47:59 -0500

    testing ls for java

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -36,3 +36,13 @@ lspconfig.clangd.setup {
     }
   }
 }
+
+lspconfig.java.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  init_options = {
+    preferences = {
+      disableSuggestions = false,
+    }
+  }
+}
