864800af8dc92e7da73554365b67bf752b10295c
EthanPayne1 <payne.ethan6@yahoo.com>
2024-10-20 18:35:57 -0500

    copied nested null-lsp into root

--- nvim/lua/configs/null-ls.lua
+++ nvim/lua/configs/null-ls.lua
@@ -5,5 +5,20 @@ local opts = {
   sources = {
     null_ls.builtins.formatting.clang_format,
   },
-  on_attch
+  on_attach = function(client, bufnr)
+    if client.supports_methods("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({
+        group = augroup,
+        buffer = bufnr,
+      })
+      vim.api.nvim_create_autocmd("BuffWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr })
+        end,
+      })
+    end
+  end,
 }
+return opts
